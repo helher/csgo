1d2f98d6811d6d21727da4e95519dc127ceaba32
Helena Hermansen <helena.hermansen@outlook.com>
2018-10-17 19:44:19 +0200

    Major Changes

--- helena/main.js
+++ helena/main.js
@@ -5,3 +5,11 @@ function GetValue()
     //alert(random);
     document.getElementById("message").innerHTML=random;
 }
+
+function GetJoker()
+{
+    var jokerMaps= ["Cache","Mirage","Overpass","Inferno","Austria","Office","Agency","Dust II", "Nuke", "Train", "Potatotes was here"];
+    var random = jokerMaps[Math.floor(Math.random() * jokerMaps.length)];
+    //alert(random);
+    document.getElementById("message").innerHTML=random;
+}
